a32b63dbefc551390031d9bb9af08c98e243cde6
Feyisayo <feyisayo233@gmail.com>
2025-09-28 13:36:49 +0100

    Add SearchBox component and implement search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header.jsx";
 import ThemeCom from "@/components/ThemeCom.jsx";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
           <ThemeCom>
             <Header />
             <Navbar />
+            <SearchBox />
             {children}
           </ThemeCom>
         </body>
